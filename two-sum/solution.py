c8d2bb91f701d6f46988f6840932ff82d5d230c9
Aayush Gandhi <66742440+AayushGithub@users.noreply.github.com>
2023-07-03 14:07:50 +0530

    Two Sum solution added

---
+++ two-sum/solution.py
@@ -0,0 +1,12 @@
+# Link - https://leetcode.com/problems/two-sum/
+# Question ID - 1
+# Question Name - Two Sum
+class Solution(object):
+    def twoSum(self, nums, target):
+        checkList = {}
+        for i in range(len(nums)):
+            if (target - nums[i]) in checkList:
+                return [checkList[target-nums[i]], i] 
+            checkList[nums[i]] = i
+        return []
+            
