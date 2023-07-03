ee2bfe00254bba81e9d5e28b92859b1bd44e9c38
Aayush Gandhi <66742440+AayushGithub@users.noreply.github.com>
2023-07-03 15:19:54 +0530

    Ulgy Number Solution Added

---
+++ ugly-number/solution.py
@@ -0,0 +1,14 @@
+# Link - https://leetcode.com/problems/ugly-number/
+# Question ID - 263
+# Question Name - Ugly Number
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 2 == 0:
+            n //= 2
+        while n % 3 == 0:
+            n //= 3
+        while n % 5 == 0:
+            n //= 5
+        return n == 1
